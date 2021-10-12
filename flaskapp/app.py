c5bf4d307e75266715b03136b8da1f8912bf8521
Евгений Герасин <e.gerasin@bars-open.ru>
2021-10-12 15:34:17 +0300

    master
    Добавлен демо версия API

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, jsonify
 from flask.templating import render_template
 
 import os
@@ -59,3 +59,39 @@ def settings():
         'number': 1,
     }
     return render_template('settings.html', data=context)
+
+
+@app.route('/api/random')
+def api_random():
+
+    r = RandomUserData()
+    if len(request.args) == 0:
+        resp = {
+            'status': True,
+            'users': [
+                r.full_random_user()
+            ]
+        }
+        return jsonify(resp)
+
+    date_from = request.args.get('date_from')
+    date_to = request.args.get('date_to')
+    gender = request.args.get('gender')
+    number = request.args.get('number')
+
+    if number:
+        users = []
+        try:
+            amount = int(number)
+            for _ in range(0, amount):
+                users.append(r.full_random_user())
+                resp = {
+                    'status': True,
+                    'users': users,
+                }
+        except:
+            resp = {
+                'status': False,
+                'test error': 'Number users is wrong. For example try /api/random?number=33'
+            }
+    return jsonify(resp)
