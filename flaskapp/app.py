5f7dcb1f69e0e677aabd4e9d3030be91bff6ca84
Евгений Герасин <e.gerasin@bars-open.ru>
2021-10-13 11:25:47 +0300

    master
    Добавлен основной функционал API

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,7 +1,9 @@
+import re
 from flask import Flask, request, send_from_directory, jsonify
 from flask.templating import render_template
 
 import os
+import time
 
 from flaskapp.engine.random import RandomUserData
 
@@ -65,6 +67,7 @@ def settings():
 def api_random():
 
     r = RandomUserData()
+    # if not one parameters return full random user
     if len(request.args) == 0:
         resp = {
             'status': True,
@@ -79,19 +82,75 @@ def api_random():
     gender = request.args.get('gender')
     number = request.args.get('number')
 
-    if number:
+    if date_from and date_to:
+
+        # check format date
+        date_from_cond = re.match(r'[0-9]{4}-[0-9]{2}-[0-9]{2}', date_from)
+        date_to_cond = re.match(r'[0-9]{4}-[0-9]{2}-[0-9]{2}', date_to)
+        if not date_from_cond or not date_to_cond:
+            resp = {
+                'status': False,
+                'test error': 'The date format must be YYYY-mm-dd. For example try /api/random?date_from=1990-01-01&date_to=2010-02-02'
+            }
+            return jsonify(resp)
+        # check difference date
+        try:
+            d_f = time.mktime(time.strptime(date_from, '%Y-%m-%d'))
+            d_t = time.mktime(time.strptime(date_to, '%Y-%m-%d'))
+        except ValueError:
+            resp = {
+                'status': False,
+                'test error': 'The date format must be YYYY-mm-dd. For example try /api/random?date_from=1990-01-01&date_to=2010-02-02'
+            }
+            return jsonify(resp)
+        if d_f > d_t:
+            resp = {
+                'status': False,
+                'test error': 'Date from must be earlier than date to'
+            }
+            return jsonify(resp)
+        # application of dates
+        r.start_date = date_from
+        r.stop_date = date_to
+    # if dates not get
+    elif date_to and not date_from or date_from and not date_to:
+        resp = {
+            'status': False,
+            'test error': 'Date from and date to should be transferred together'
+        }
+        return jsonify(resp)
+    # check gender
+    if gender == 'male' or gender == 'female':
+        r.gender = gender
+    else:
+        resp = resp = {
+            'status': False,
+            'test error': 'Gender users is wrong. Gender should be male or female. For example try /api/random?gender=male or /api/random?gender=female'
+        }
+        return jsonify(resp)
+    # check number users generation
+    if number or number == '':
         users = []
         try:
             amount = int(number)
-            for _ in range(0, amount):
-                users.append(r.full_random_user())
-                resp = {
-                    'status': True,
-                    'users': users,
-                }
-        except:
+        except ValueError:
             resp = {
                 'status': False,
                 'test error': 'Number users is wrong. For example try /api/random?number=33'
             }
-    return jsonify(resp)
+            return jsonify(resp)
+        # check if number == 0
+        if amount < 1:
+            resp = {
+                'status': False,
+                'test error': 'Number users is wrong. Should be > 0'
+            }
+            return jsonify(resp)
+        # * main function
+        for _ in range(0, amount):
+            users.append(r.castom_random_user())
+            resp = {
+                'status': True,
+                'users': users,
+            }
+        return jsonify(resp)
